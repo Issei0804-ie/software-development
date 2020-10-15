e5aa9de50d63f8d1e791ff4f78770261b84eccec
宮良一生 <e195755@ie.u-ryukyu.ac.jp>
2020-10-15 11:07:13 +0900

    add create method

--- DBTEST/database/database.go
+++ DBTEST/database/database.go
@@ -11,26 +11,25 @@ type Database struct {
 	DB *gorm.DB
 }
 
-func NewDBHandler() *Database{
-	var DBMS, DBUSER, PASS, PROTOCOL,DBNAME string
+func NewDBHandler() *Database {
+	var DBMS, DBUSER, PASS, PROTOCOL, DBNAME string
 
-	if os.Getenv("MODE") == "TEST"{
-		DBMS     = os.Getenv("TEST_DBMS")
-		DBUSER   = os.Getenv("TEST_DBUSER")
-		PASS     = os.Getenv("TEST_PASS")
+	if os.Getenv("MODE") == "TEST" {
+		DBMS = os.Getenv("TEST_DBMS")
+		DBUSER = os.Getenv("TEST_DBUSER")
+		PASS = os.Getenv("TEST_PASS")
 		PROTOCOL = os.Getenv("TEST_PROTOCOL")
-		DBNAME   = os.Getenv("TEST_DBNAME")
-	}else if os.Getenv("MODE") == "RELEASE"{
-		DBMS     = os.Getenv("DBMS")
-		DBUSER   = os.Getenv("DBUSER")
-		PASS     = os.Getenv("PASS")
+		DBNAME = os.Getenv("TEST_DBNAME")
+	} else if os.Getenv("MODE") == "RELEASE" {
+		DBMS = os.Getenv("DBMS")
+		DBUSER = os.Getenv("DBUSER")
+		PASS = os.Getenv("PASS")
 		PROTOCOL = os.Getenv("PROTOCOL")
-		DBNAME   = os.Getenv("DBNAME")
+		DBNAME = os.Getenv("DBNAME")
 	}
 
-	CONNECT := DBUSER+":"+PASS+"@"+PROTOCOL+"/"+DBNAME +"?parseTime=true"
-	db,err := gorm.Open(DBMS,CONNECT)
-
+	CONNECT := DBUSER + ":" + PASS + "@" + PROTOCOL + "/" + DBNAME + "?parseTime=true"
+	db, err := gorm.Open(DBMS, CONNECT)
 
 	if err != nil {
 		panic(err.Error())
@@ -40,10 +39,17 @@ func NewDBHandler() *Database{
 	}
 }
 
+func (d *Database) GetUserByID(id int) (user User, err error) {
+	err = d.DB.Where("id = ?", id).Find(&user).Error
+	if err != nil {
+		log.Println("error:" + err.Error())
+	}
+	return
+}
 
-func (d *Database)GetUserByID(id int)(user User){
-	err := d.DB.Where("id = ?",id).Find(&user).Error
-	if err != nil{
+func (d *Database) CreateUser(user User) (err error) {
+	err = d.DB.Create(&user).Error
+	if err != nil {
 		log.Println("error:" + err.Error())
 	}
 	return
